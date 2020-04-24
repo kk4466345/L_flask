59a7c07d5fb8ea93132ef5637d1238f98a8d75e4
kk4466345 <kk4466345@gmail.com>
2020-04-25 02:09:41 +0530

    cookie finished

--- script.py
+++ script.py
@@ -1,16 +1,12 @@
-
 from flask import *  
+  
 app = Flask(__name__)  
+ 
+@app.route('/cookie')  
+def cookie():  
+    res = make_response("<h1>cookie is set</h1>")  
+    res.set_cookie('foo','bar')  
+    return res  
   
-@app.route('/')  
-def customer():  
-   return render_template('customer.html')  
-  
-@app.route('/success',methods = ['POST', 'GET'])  
-def print_data():  
-   if request.method == 'POST':  
-      result = request.form  
-      return render_template("result_data.html",result = result)  
-   
 if __name__ == '__main__':  
-   app.run(debug = True)  
+    app.run(debug = True)  
