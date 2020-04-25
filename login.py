191a76a85e2e64f6d9e26ebf9a0205ae67968809
kk4466345 <kk4466345@gmail.com>
2020-04-25 14:51:32 +0530

    error and redirect the url

--- login.py
+++ login.py
@@ -1,43 +1,24 @@
 from flask import *  
-  
 app = Flask(__name__)  
-app.secret_key = "ayush"
-@app.route('/error')  
-def error():  
-    return "<p><strong>Enter correct password</strong></p>"  
  
 @app.route('/')  
-def home():  
-    return render_template("home.html")
-
-@app.route('/login')
-def login():
-    return render_template("login.html")
-
-@app.route('/success',methods = ['POST'])  
-def success():  
-    if request.method == "POST":  
-        session['email']=request.form['email']
-
-    return render_template('success.html')  
-
-
-@app.route('/logout')  
-def logout():  
-    if 'email' in session:  
-        session.pop('email',None)  
-        return render_template('logout.html');  
-    else:  
-        return '<p>user already logged out</p>'  
-
-
-@app.route('/profile')  
-def profile():  
-    if 'email' in session:  
-        email = session['email']  
-        return render_template('profile.html',name=email)  
-    else:  
-        return '<p>Please login first</p>'  
+def home ():  
+    return render_template("home.html")  
  
-if __name__ == "__main__":  
+@app.route('/login')  
+def login():  
+    return render_template("login.html");  
+ 
+@app.route('/validate', methods = ["POST"])  
+def validate():  
+    if request.method == 'POST' and request.form['pass'] == 'jtp':  
+        return redirect(url_for("success"))
+    else:
+        abort(401)
+ 
+@app.route('/success')  
+def success():  
+    return "logged in successfully"  
+  
+if __name__ == '__main__':  
     app.run(debug = True)
